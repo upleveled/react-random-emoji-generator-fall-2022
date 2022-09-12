96be07b8376697aa105e5f5ed26e9724c930b16c
Karl Horky <karl.horky@gmail.com>
2022-09-12 13:37:54 +0200

    Implement app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import nodeEmoji from 'node-emoji';
+import { useState } from 'react';
 
 function App() {
+  const [emoji, setEmoji] = useState(nodeEmoji.random().emoji);
+  const [emojiName, setEmojiName] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random Emoji Generator</h1>
+      <div
+        style={{
+          width: 400,
+          height: 400,
+          margin: '0 auto',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 80,
+          border: '1px solid purple',
+          borderRadius: 25,
+        }}
+      >
+        {emoji}
+      </div>
+
+      <button
+        onClick={() => {
+          const newEmoji = nodeEmoji.random().emoji;
+          setEmoji(newEmoji);
+        }}
+      >
+        Generate
+      </button>
+
+      <br />
+      <br />
+
+      {/* Controlled component */}
+      <input
+        // 2. Use state variable
+        value={emojiName}
+        // 3. Update the state variable
+        onChange={(event) => {
+          setEmojiName(event.currentTarget.value);
+
+          const chosenEmoji = nodeEmoji.find(event.currentTarget.value)?.emoji;
+          if (chosenEmoji) {
+            setEmoji(chosenEmoji);
+          }
+        }}
+      />
+
+      {
+        // Did the user type something in the box?
+        emojiName &&
+          // Was an emoji NOT found
+          !nodeEmoji.hasEmoji(emojiName) &&
+          // If both of these things are true, then show this message:
+          'Emoji not found'
+      }
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
